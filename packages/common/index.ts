4dbac7c0d66a4862d3e7ea556195f870057e7701
Nishant Singh <nraj14908@gmail.com>
2026-09-03 21:44:00 +0530

    add Zod schemas for input validation

---
+++ packages/common/index.ts
@@ -0,0 +1,22 @@
+import z from "zod";
+
+// just for the input ( so we add zod for the input type validation)
+export const CreateWorkspaceSchema = z.object({
+    path: z.string(),
+})
+
+// creates a TypeScript type automatically from the Zod schema
+export type CreateWorkspaceSchemaType = z.infer<typeof CreateWorkspaceSchema>
+
+export const CreateSessionSchema = z.object({
+    workspaceId: z.string()
+})
+// infer -> create a typescript type
+export type CreateSessionSchemaType = z.infer<typeof CreateSessionSchema>
+
+export const AddMessageSchema = z.object({
+    sessionId: z.string(),
+    message: z.string()
+})
+
+export type AddMessageSchemaType = z.infer<typeof AddMessageSchema>
